96081845dc80d62c5be275aa24095000d666dbd3
seungminng <ssmmkim1025@naver.com>
2026-09-03 13:47:23 +0900

    🔀 메인 페이지 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
-export default function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import HomePage from "@/pages/home/HomePage";
+import LibraryPage from "@/pages/library/LibraryPage";
+import MyPage from "@/pages/my/MyPage";
+import TrainingPage from "@/pages/training/TrainingPage";
+import VocabularyPage from "@/pages/vocabulary/VocabularyPage";
+
+function App() {
   return (
-    <main className="min-h-screen">
-      <h1>여백</h1>
-    </main>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/vocabulary" element={<VocabularyPage />} />
+        <Route path="/training" element={<TrainingPage />} />
+        <Route path="/library" element={<LibraryPage />} />
+        <Route path="/my" element={<MyPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 
 import App from "@/App";
 import "@/index.css";
@@ -13,10 +12,7 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* TanStack Query 설정, 서버에서 받아오는 데이터관리 라이브러리*/}
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-
+      <App />
       {/* React Query 개발 도구, 캐시된 데이터, API 호출 정보 확인가능 */}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
