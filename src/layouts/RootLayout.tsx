d69786568ab7db15fa576e8b310c5eedf751bc2c
seungminng <ssmmkim1025@naver.com>
2026-09-08 10:43:48 +0900

    🎨 style: 전역 배경색 변경

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -4,7 +4,7 @@ import FooterNavigation from "@/components/common/footerNavigation/FooterNavigat
 
 export default function RootLayout() {
   return (
-    <div className="mx-auto min-h-screen w-full max-w-97.5 px-5 py-5 bg-white">
+    <div className="mx-auto min-h-screen w-full max-w-97.5 px-5 py-5 bg-[#F7F6F1]">
       <Outlet />
       <FooterNavigation />
     </div>
