85581982b61bf3b7d0d4a1c832df229ba05caf9d
seungminng <ssmmkim1025@naver.com>
2026-09-08 09:40:10 +0900

    ✨ Feat: 헤더 레이아웃에 추가

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 
 import FooterNavigation from "@/components/common/footerNavigation/FooterNavigation";
+import Header from "@/components/common/header/Header";
 
 export default function RootLayout() {
   return (
     <div className="mx-auto min-h-screen w-full max-w-97.5 px-5 py-5 bg-white">
+      <Header />
       <Outlet />
       <FooterNavigation />
     </div>
