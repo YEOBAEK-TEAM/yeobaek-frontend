679af672b3ff0aaa53cb6d221471c7b3b7686935
seungminng <ssmmkim1025@naver.com>
2026-09-08 10:41:42 +0900

    🐛 fix: 헤더 우측 아이콘 정렬 방식 수정

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -12,7 +12,7 @@ export default function Header({ title, action }: HeaderProps) {
     <div className="h-17 mt-10 flex gap-2">
       <img src={LityIcon} alt="리티짱" className="h-11 w-11" />
       <p className="font-extrabold pt-2 text-3xl text-[#4F4D4E]">{title}</p>
-      <div className="absolute right-5 mt-1">
+      <div className="ml-auto mt-1">
         {action == "bell" && (
           <button type="button">
             <img src={BellIcon} className="h-10 w-10 cursor-pointer"></img>
