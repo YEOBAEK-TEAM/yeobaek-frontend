c15fdf4220b324eb363f3e5d640b7f58f006a98d
seungminng <ssmmkim1025@naver.com>
2026-09-08 10:26:17 +0900

    ✨ feat: 공통 헤더 컴포넌트 구현

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,3 +1,29 @@
-export default function Header() {
-  return <h1> 헤더</h1>;
+import LityIcon from "@/assets/icons/LityIcon.png";
+import BellIcon from "@/assets/icons/BellIcon.png";
+import SearchIcon from "@/assets/icons/SearchIcon.png";
+
+type HeaderProps = {
+  title: string;
+  action?: "bell" | "search";
+};
+
+export default function Header({ title, action }: HeaderProps) {
+  return (
+    <div className="h-17 mt-10 flex gap-1 ">
+      <img src={LityIcon} alt="리티짱" className="h-11 w-11" />
+      <p className="font-extrabold pt-2 text-3xl text-[#4F4D4E]">{title}</p>
+      <div className="absolute right-5 mt-1">
+        {action == "bell" && (
+          <button type="button">
+            <img src={BellIcon} className="h-10 w-10 cursor-pointer"></img>
+          </button>
+        )}
+        {action == "search" && (
+          <button type="button">
+            <img src={SearchIcon} className="h-10 w-10 cursor-pointer"></img>
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
 
 import FooterNavigation from "@/components/common/footerNavigation/FooterNavigation";
-import Header from "@/components/common/header/Header";
 
 export default function RootLayout() {
   return (
     <div className="mx-auto min-h-screen w-full max-w-97.5 px-5 py-5 bg-white">
-      <Header />
       <Outlet />
       <FooterNavigation />
     </div>
