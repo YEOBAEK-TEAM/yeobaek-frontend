73f31e2d083dd7c53195eec6e6c2c0d17adf9553
seungminng <ssmmkim1025@naver.com>
2026-09-15 11:02:22 +0900

    🐛 fix: 미사용 ReactQueryDevtools import 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
