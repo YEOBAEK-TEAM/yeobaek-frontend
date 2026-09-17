03ff233f5db45f9d3f606e0bfde0ba4e21cf0c79
sowho.j <jangseohu112@gmail.com>
2026-09-17 13:16:45 +0900

    ✨ Feat: 훈련 목록 상수 및 목데이터 추가

---
+++ src/constants/training/ongoingTraining.ts
@@ -0,0 +1,19 @@
+import CompleteCharacter from "@/assets/images/Training/CompleteCharacter.png";
+import ComprehensiveingCharacter from "@/assets/images/Training/ComprehensiveingCharacter.png";
+import NothingcCharcter from "@/assets/images/Training/NothingcCharcter.png";
+import TrainingCharacter from "@/assets/images/Training/TrainingCharacter.png";
+
+// 이미지마다 여백과 비율이 달라 크기/위치를 상태별로 지정
+export const ONGOING_TRAINING_CHARACTER = {
+  bookReport: { src: TrainingCharacter, className: "h-36 -right-4 bottom-0" },
+  comprehension: { src: ComprehensiveingCharacter, className: "h-36 -right-4 bottom-0" },
+  completed: { src: CompleteCharacter, className: "h-28 right-0 -bottom-1" },
+  empty: { src: NothingcCharcter, className: "h-40 -right-4 -bottom-4" },
+};
+
+export const ONGOING_TRAINING_LABEL = "지금 훈련 중";
+
+export const EMPTY_ONGOING_TRAINING = {
+  title: "아직 훈련 기록이 없습니다",
+  description: "리티와 함께 생각의 풀을 넓혀봐요",
+};
